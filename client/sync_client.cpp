55adf813e38d4ffd513059a1ff4adf7265100b70
triplesheep <18811464290@163.com>
2018-04-12 20:29:59 +0800

    Add sync client test

--- client/sync_client.cpp
+++ client/sync_client.cpp
@@ -1,8 +1,8 @@
 /*
 * @Author: dummy
 * @Date:   2018-04-09 13:39:25
-* @Last Modified by:   dummy
-* @Last Modified time: 2018-04-09 15:51:39
+* @Last Modified by:   triplesheep
+* @Last Modified time: 2018-04-12 20:26:16
 */
 #include "sync_client.h"
 #include <sys/types.h>
@@ -10,7 +10,11 @@
 #include <unistd.h>
 #include <assert.h>
 #include <errno.h>
-
+#include <netinet/in.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <iostream>
 
 SyncClient::SyncClient() : _sockfd(-1) , _connected(false) { }
 
@@ -24,12 +28,12 @@ void SyncClient::release() {
     }
 }
 
-int SyncClient::build_connect(char* ip, int port) {
+int SyncClient::build_connect(const char* ip, int port) {
     if (port == 0 || !ip)  return -1;
     struct sockaddr_in server_addr;
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    inet_pton(AF_INET, ip, server_addr.sin_addr);
+    inet_pton(AF_INET, ip, &server_addr.sin_addr);
     server_addr.sin_port = htons(port);
 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -42,33 +46,37 @@ int SyncClient::build_connect(char* ip, int port) {
     return _sockfd;
 }
 
-int SyncClient::send_data(int sockfd, void* data) {
+int SyncClient::send_data(void* data) {
     assert(_connected);
     char* data_to_send = static_cast<char*>(data);
-    int ret = send(sockfd, data_to_send, sizeof(data_to_send), 0);
+    int ret = send(_sockfd, data_to_send, strlen(data_to_send), 0);
     if (ret < 0)
         return -1;
     return ret;
 }
 
-int SyncClient::recv_data(int sockfd, void* buff) {
+int SyncClient::recv_data(void* buff, int buff_len) {
     assert(_connected);
     char* buff_to_recv = static_cast<char*>(buff);
-    int ret = recv(sockfd, buff_to_recv, sizeof(buff_to_recv) - 1, 0);
+    int ret = recv(_sockfd, buff_to_recv, buff_len - 1, 0);
+    if (ret == 0) {
+        close(_sockfd);
+        _connected = false;
+        return 0;
+    }
     if (ret < 0)
         return -1;
     return ret;
 }
 
-int SyncClient::commuicate_process(int sockfd, void* data, void* buff, void* callback) {
+int SyncClient::commuicate_process(void* data, void* buff, int buff_len) {
     assert(_connected);
-    assert(!callback);
-    int ret = send_data(sockfd, data);
+    int ret = send_data(data);
     if (ret < 0)
         return -1;
     if (!buff)
         return ret;
-    ret = recv(sockfd, buff);
+    ret = recv_data(buff, buff_len);
     if (ret < 0)
         return -1;
     return ret;
--- client/sync_client.h
+++ client/sync_client.h
@@ -1,21 +1,21 @@
 /*
 * @Author: dummy
 * @Date:   2018-04-09 13:34:30
-* @Last Modified by:   dummy
-* @Last Modified time: 2018-04-09 15:51:11
+* @Last Modified by:   triplesheep
+* @Last Modified time: 2018-04-12 20:26:32
 */
 #ifndef SYNC_CLIENT_H
 #define SYNC_CLIENT_H
 
-#include "client_base.h"
 
 class SyncClient {
 public:
-    ClientBase();
-    int build_connect(char* ip, int port);
-    int send_data(int sockfd, void* data);
-    int recv_data(int sockfd, void* buff);
-    int commuicate_process(int sockfd, void* data, void* buff, void* callback);
+    SyncClient();
+    ~SyncClient();
+    int build_connect(const char* ip, int port);
+    int send_data(void* data);
+    int recv_data(void* buff, int buff_len);
+    int commuicate_process(void* data, void* buff, int buff_len);
     void release();
 private:
     int _sockfd;
